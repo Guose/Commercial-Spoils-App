cd7f54370793c0a0e324a3dc105540988ae8af66
Guose <guose79@gmail.com>
2016-01-08 16:31:10 -0800

    Update single data type to long

---
+++ Spoils.Scan/ScanRecords.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoils.Scan
+{
+    class ScanRecords
+    {
+        #region Constructors
+
+        public ScanRecords()
+        {
+
+        }
+
+        public ScanRecords(string firstNum, string lastNum) : this()
+        {
+            FirstNumberScanned = firstNum;
+            LastNumberScanned = lastNum;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+        private string _singleScan;
+
+        public string SingleScan
+        {
+            get { return _singleScan; }
+            set { _singleScan = value; }
+        }
+        private string _firstNumberScanned;
+
+        public string FirstNumberScanned
+        {
+            get { return _firstNumberScanned; }
+            set { _firstNumberScanned = value; }
+        }
+        private string _lastNumberScanned;
+
+        public string LastNumberScanned
+        {
+            get { return _lastNumberScanned; }
+            set { _lastNumberScanned = value; }
+        }
+        #endregion Properties
+
+        internal class Scan
+        {
+            public string Scann { get; set; }
+        }
+
+        internal class Scans
+        {
+            public List<Scan> _scans;
+
+            public Scans()
+            {
+                _scans = new List<Scan>();
+
+            }
+            //AddSingle method to instantiate new object of Scan
+            public void AddSingle(string scan)
+            {
+                _scans.Add(new Scan { Scann = scan });
+            }
+        }
+
+    }
+}
---
+++ Spoils.Scan/ScanService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Spoils.Scan
+{
+    public class ScanService
+    {
+        public ScanService()
+        {
+
+        }
+        
+        internal string Scan { get; private set; }
+        internal bool IsSingle { get; private set; }
+        internal ScanRecords.Scans Scans { get; private set; }
+
+        public bool ProcessScan(string scan, bool isSingle)
+        {
+            //logic
+            //return false;
+            
+            Scans.AddSingle(scan);
+
+            return true;
+        }
+
+        public bool Finalize()
+        {
+            foreach (var scan in Scans._scans)
+            {
+
+            }
+            return true;
+        }
+    }
+
+}
